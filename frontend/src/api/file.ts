258481ada65be4d9b7dcf57c1ed081128a368f5b
Martin Wundsam <github@wundsam.icu>
2022-10-31 17:30:11 +0100

    introduce api url

--- frontend/src/api/file.ts
+++ frontend/src/api/file.ts
@@ -3,7 +3,7 @@ import type { CustomFilesResponse, MetaData, MetaDataResponse } from '../types/f
 
 export class FileApi {
 
-    static BASE_URL = "http://" + window.location.hostname + import.meta.env.VITE_API_PORT
+    static BASE_URL = import.meta.env.VITE_API_URL?.length > 0 ? import.meta.env.VITE_API_URL : "http://" + window.location.hostname + import.meta.env.VITE_API_PORT
 
     static async uploadMetadata(data: MetaData): Promise<MetaDataResponse> {
         console.log(this.BASE_URL + "/upload/meta")
